38ec866c7c412b436e8637c3b5ce1b0281600171
Boofny <Brandonpizano7557@gmail.com>
2026-03-05 20:41:14 -0500

    some cool work will probably work on making the python script into an ffmpeg command

--- GolangImage/tcpserver.go
+++ GolangImage/tcpserver.go
@@ -3,21 +3,28 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
-
-	// "flag"
+	"flag"
 	"fmt"
 	"image"
 	"image/jpeg"
 	"io"
 	"log"
 	"net"
+	"strings"
 )
 
-// so this works with netcat but not with the currect example of ./clientExample.go
+// golang perfomince changes 
+/*
+Number one made a bufferd chanel for the image 
+Made a seperate goroutine just for the printing in order to have better printings even at large scale
+Found out about the select design pattern that skips the unused frames from a channl very cool
+*/
+var picWidth = flag.Int("s", 80, "Size of image output") // global size so any thing can use this
 
 func main() {
-	// picSize := flag.Int("s", 80, "size of ascii image in terminal")
-	// flag.Parse()
+	flag.Parse()
+
+	imgCh := make(chan string, 1)
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatal("Error listening:", err)
@@ -27,17 +34,23 @@ func main() {
 
 	fmt.Print("\033[H\033[2J") // Clear screen and move to top-left
 
+	go func ()  { // single goroutine in order to have good timing with the img channel
+		for img := range imgCh{
+			fmt.Printf("\033[%dA\n\n%s", *picWidth, img)
+		}
+	}()
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Println("Error accepting conn:", err)
 			continue
 		}
-		go handleConnection(conn)
+		go handleConnection(conn, imgCh)
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn, imgCh chan string) {
 	defer conn.Close()
 
 	for {
@@ -69,47 +82,69 @@ func handleConnection(conn net.Conn) {
 		bounds := img.Bounds()
 		width, height := bounds.Max.X, bounds.Max.Y
 
-		outputWidth := 80
+		outputWidth := *picWidth
 		outputHeight := int(float64(height) / float64(width) * float64(outputWidth) * 0.5) // Adjust aspect ratio
 		f := colorASCII(outputHeight, outputWidth, height, width, img)
-		fmt.Printf("\033[%dA", outputWidth)
-		fmt.Println("\n\n")
-		fmt.Println(f)
+		select{ // works by only running/displying if the goroutine is ready if not just skip a frame
+		case imgCh <- f:
+		default:
+		}
 	}
 }
 
-func colorASCII(outputHeight, outputWidth, height, width int, img image.Image) string {
-	// const asciiChars = "#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/()1{}[]?"
+func colorASCII(outputHeight, outputWidth, height, width int, img image.Image) string{
 	const asciiChars = "#"
-
-	// const asciiChars = "󰝤"
-	var resp string
 	resetColor := "\033[0m"
+	var sb strings.Builder
+	sb.Grow(outputHeight * outputWidth * 25)
+
 	for y := range outputHeight {
 		for x := range outputWidth {
-			// Get pixel from original image, scaled to output dimensions
 			originalX := int(float64(x) / float64(outputWidth) * float64(width))
 			originalY := int(float64(y) / float64(outputHeight) * float64(height))
 			pixel := img.At(originalX, originalY)
 			r, g, b, _ := pixel.RGBA()
-
 			gray := (r + g + b) / 3
-
-			// Map grayscale to ASCII character
 			charIndex := int(float64(gray) / 65535.0 * float64(len(asciiChars)-1))
-
-			red := r >> 8
-			green := g >> 8
-			blue := b >> 8
-
-			correctColor := fmt.Sprintf("\u001b[38;2;%d;%d;%dm", red, green, blue)
-
-			resp += fmt.Sprint(correctColor, string(asciiChars[charIndex]), resetColor)
+			fmt.Fprintf(&sb, "\u001b[38;2;%d;%d;%dm", r>>8, g>>8, b>>8)
+			sb.WriteByte(asciiChars[charIndex])
+			sb.WriteString(resetColor)
 		}
-		resp += "\n"
+		sb.WriteByte('\n')
 	}
-	return resp
+  return sb.String()
 }
+
+// func colorASCII(outputHeight, outputWidth, height, width int, img image.Image) string {
+// 	// const asciiChars = "#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/()1{}[]?"
+// 	const asciiChars = "#"
+// 	var resp string
+// 	resetColor := "\033[0m"
+// 	for y := range outputHeight {
+// 		for x := range outputWidth {
+// 			// Get pixel from original image, scaled to output dimensions
+// 			originalX := int(float64(x) / float64(outputWidth) * float64(width))
+// 			originalY := int(float64(y) / float64(outputHeight) * float64(height))
+// 			pixel := img.At(originalX, originalY)
+// 			r, g, b, _ := pixel.RGBA()
+//
+// 			gray := (r + g + b) / 3
+//
+// 			// Map grayscale to ASCII character
+// 			charIndex := int(float64(gray) / 65535.0 * float64(len(asciiChars)-1))
+//
+// 			red := r >> 8
+// 			green := g >> 8
+// 			blue := b >> 8
+//
+// 			correctColor := fmt.Sprintf("\u001b[38;2;%d;%d;%dm", red, green, blue)
+//
+// 			resp += fmt.Sprint(correctColor, string(asciiChars[charIndex]), resetColor)
+// 		}
+// 		resp += "\n"
+// 	}
+// 	return resp
+// }
 
 // og way to handle tcp server
 //
